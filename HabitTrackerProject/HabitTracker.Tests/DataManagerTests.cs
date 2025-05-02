6c38caa879c3e979b81e5bc2eea1137d703c925e
emmaduru <99149024+emmaduru@users.noreply.github.com>
2025-05-02 11:53:34 +0000

    add tests for datamanager

---
+++ HabitTrackerProject/HabitTracker.Tests/DataManagerTests.cs
@@ -0,0 +1,39 @@
+namespace HabitTracker.Tests;
+using HabitTracker;
+
+public class DataManagerTests
+{
+    DataManager dataManager;
+    List<string> testEntries;
+    public DataManagerTests () {
+        testEntries = new List<string>();
+        testEntries.Add("Goal1,1,1");
+        testEntries.Add("Goal2,2,3");
+        testEntries.Add("Goal3,4,5");
+        File.WriteAllLines("habits.txt", testEntries);
+        dataManager = new DataManager();
+    }
+
+    [Fact]
+    public void Test_DataManager_Initialization()
+    {
+        Assert.Equal(3, dataManager.Habits.Count);
+    }
+
+    [Fact]
+    public void Test_DataManager_AddHabit()
+    {
+        dataManager.AddHabit(new Habit("New Habit"));
+        Assert.Equal(4, dataManager.Habits.Count);
+    }
+
+    [Fact]
+    public void Test_DataManager_UpdateHabit()
+    {
+        Habit newHabit = new Habit("Goal3");
+        newHabit.updateGoal(1000);
+        dataManager.updateHabit(newHabit);
+        int habitIndex = dataManager.Habits.FindIndex(t => t == newHabit);
+        Assert.Equal(true, habitIndex != -1);
+    }
+}
